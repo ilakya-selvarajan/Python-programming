6854e5bb6845213424c9a45a95535882e996f7f1
ilakya-selvarajan <ilakya.selvarajan@gmail.com>
2015-02-03 22:28:46 +0200

    second draft

--- Palindromes/rp.py
+++ Palindromes/rp.py
@@ -6,6 +6,7 @@ def rp(seq,min_len,max_len):
     length = len(seq)
     list1 = []
     seq_sub= []
+    
     index1=[]
     index2=[]
     seq1=str(seq)
@@ -20,17 +21,29 @@ def rp(seq,min_len,max_len):
             reverse=Seq(substring).reverse_complement()
             #rev_comp.append(str(reverse))
             if substring==str(reverse):
-                seq_sub.append(substring)
-    print seq_sub
+                index1.append(substring)
+                seq_sub.append((len(substring),substring,seq1.find(substring)))
+                
     
-    for i in range(len(seq_sub)  -1, -1, -1):
-        if seq_sub[i].find(seq_sub[i-1])==True:
-              print seq_sub[i+1]
-              #seq_sub.remove(seq_sub[i-1])
-        if seq_sub[i-1].find(seq_sub[i])==True:
-            print seq_sub[i]
-            #seq_sub.remove(seq_sub[i])
-    print seq_sub        
+    seq_sub1= sorted(seq_sub)
+    print seq_sub1
+    for i in range(len(seq_sub1) - 1, -1, -1):
+        (len1,str1,idx1)=seq_sub1[i]
+        for j in range(0,i):
+            (len2,str2,idx2)=seq_sub1[j]
+            #print strstr1
+            if (idx2>=idx1) and (idx2<=(idx1+len1)) and ((idx2+len2)<=(idx1+len1)):
+                index2.append(str2)
+    print list(set(index1).difference(index2))
+    # for i in range(0,len(seq_sub)-1):
+        # if seq_sub[i].find(seq_sub[i+1])!=True:
+            # print i,i+1
+            # if seq_sub[i+1].find(seq_sub[i])!=True:
+                # index2.append(seq_sub[i])
+    # for seq in seq_sub:
+        # if seq
+            # #seq_sub.remove(seq_sub[i])
+    # print index2        
          
            
 substrings=[]
