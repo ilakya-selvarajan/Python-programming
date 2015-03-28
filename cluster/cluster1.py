cf034c29f9bb610097163fde79a560e80eaad6d4
ilakya-selvarajan <ilakya.selvarajan@gmail.com>
2015-03-28 20:27:35 +0200

    done :)

--- cluster/cluster1.py
+++ cluster/cluster1.py
@@ -3,31 +3,45 @@ from Bio.Cluster import *
 from Bio import Phylo
 
 
-f1=[]
-names=[]
 def tonewick(node,tree,words):
-    order1,order2=[],[]
+    
     nNodes = len(tree) 
     nElements = nNodes + 1 
     neworder = numpy.zeros(nElements)
-    clusterids = numpy.arange(nElements)    
-    for i in range(nNodes): 
+    #tuple1=()
+    list1=[4,5]
+    list2=[] 
+    
+    for i in xrange(len(tree)-1, -1, -1):
            i1 = tree[i].left 
-           i2 = tree[i].right 
-           
-           while i1 < 0: 
-                i1 = i1+1
-                i1 = tree[i].left 
-           else: 
-               order2.append(words[i1])
-               print words[i1], words[i1-1]
-           # if i2 < 0: 
-               # order2 = nodeorder[-i2-1] 
-               # count2 = nodecounts[-i2-1] 
-           # else: 
-               # order2 = node[i2] 
-               # count2 = 1 
-    print order2
+           i2 = tree[i].right
+           if i1<=0 and  i2<=0:
+                if i1==i2+1 or i1==i2-1:
+                    if i2+1==i1:
+                        #print (words[i1],words[i2]),i1+1
+                        tuple1=(words[i1],words[i2])
+                        list2.append(tuple1)
+                        list3.append(words[i1+1])
+                        clusterids.remove(i1)
+                        clusterids.remove(i2)
+           if i1>=0 and i2>=0: 
+                if i1==i2+1 or i1==i2-1:
+                    if i2+1==i1:
+                        #print (words[i1],words[i2]),i1+1
+                    #print (words[i1],words[i2]),(i1,i2)
+                        tuple1=(words[i1],words[i2])
+                        list2.append(tuple1)
+                        list3.append(words[i1+1])
+                        clusterids.remove(i1)
+                        clusterids.remove(i2)
+             
+    
+    #print order2
+    return list2,list3
+clusterids = range(-21, 22)
+f1=[]
+names=[]
+list3=[]
     
 f = gzip.open('words.txt.gz', 'rb')
 file_content = f.readlines()
@@ -53,11 +67,10 @@ for fst in f1:
 dict22={}  
 dict23={}        
 data1=kcluster(f3,nclusters=7,method='a',dist='e')
-print data1
+#print data1
 list11=data1[0]
 for i in range (0,len(list11)):
     dict22[names[i]]=list11[i]
-
 
 for keys,values in dict22.iteritems():
     try:
@@ -67,12 +80,13 @@ for keys,values in dict22.iteritems():
 
 #print dict23
 
-for keys,values in dict23.iteritems():
-    print "cluster",keys,"=",values
+# for keys,values in dict23.iteritems():
+    # print "cluster",keys,"=",values
 
 data2=treecluster(data=f3, mask=None, weight=None, transpose=0, dist='e', method='a', distancematrix=None)
 data2.scale()
-print data2
-print data2[0].left
-print len(data2)
-tonewick(-21,data2,names)
+
+final,number_final=tonewick(-21,data2,names)
+print zip(final,number_final)
+
+#print tuple1
